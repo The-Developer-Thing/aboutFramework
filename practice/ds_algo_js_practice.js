32390d40a90d19227b79f8bb96fe5b0d68c0c819
Nasir Khan <77237886+nasirkhn277@users.noreply.github.com>
2021-08-19 01:35:47 +0530

    node learning start

--- practice/ds_algo_js_practice.js
+++ practice/ds_algo_js_practice.js
@@ -1,25 +1,31 @@
-console.log('Jousney Start with DS Algo');
+//Linkedin JQuery
 
-function addUpTo(n){
-    let total = 0;
-    for(let i = 1; i<=n; i++){
-        total += i;
-    }
-    return total;
-}
 
-function addUpTo2(n){
-    let total = 0;
-    total = n*(n+1)/2;
-    return total;
-}
 
-let t1 = performance.now();
 
-addUpTo2(100000000000000);
 
-let t2 = performance.now();
+// console.log('Jousney Start with DS Algo');
 
-let t3 = t2-t1;
+// function addUpTo(n){
+//     let total = 0;
+//     for(let i = 1; i<=n; i++){
+//         total += i;
+//     }
+//     return total;
+// }
 
-console.log(t3/1000);
+// function addUpTo2(n){
+//     let total = 0;
+//     total = n*(n+1)/2;
+//     return total;
+// }
+
+// let t1 = performance.now();
+
+// addUpTo2(100000000000000);
+
+// let t2 = performance.now();
+
+// let t3 = t2-t1;
+
+// console.log(t3/1000);
---
+++ practice/node.js
@@ -0,0 +1,11 @@
+//node js is javascript run time enviroment on server
+
+//nodejs uses v8 that is javascript engine that used byclear
+
+console.log("Hello from nodejs");
+
+const fs = require('fs');
+
+// writeFileSync this function is used to create a file on hard drive
+fs.writeFileSync("hello.txt", "Hello from node js from Nasir");
+

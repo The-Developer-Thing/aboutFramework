3475522012e8ba58dd1380a6a8f4dbda1ceff6c4
Nasir Khan <77237886+nasirkhn277@users.noreply.github.com>
2021-08-31 04:07:15 +0530

    push

--- practice/node.js
+++ practice/node.js
@@ -2,10 +2,10 @@
 
 //nodejs uses v8 that is javascript engine that used byclear
 
-console.log("Hello from nodejs");
+// console.log("Hello from nodejs");
 
-const fs = require('fs');
+// const fs = require('fs');
 
-// writeFileSync this function is used to create a file on hard drive
-fs.writeFileSync("hello.txt", "Hello from node js from Nasir");
+// // writeFileSync this function is used to create a file on hard drive
+// fs.writeFileSync("hello.txt", "Hello from node js from Nasir");
 
